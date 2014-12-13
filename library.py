cbd1589cb926d8d80537096d951f077e4b05d373
Sean Behan <inbox@seanbehan.com>
2014-12-13 17:28:01 -0500

    set browser width,height properties

--- library.py
+++ library.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 def take_and_save_screen_shot(url):
     driver = webdriver.PhantomJS()
     driver.get(url)
+    driver.set_window_size(1200, 750)
     driver.save_screenshot("static/screenshot.png")
     driver.quit()
 
