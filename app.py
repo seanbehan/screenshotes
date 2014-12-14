a57189db028b9c641aa3f5b8afb2040556097cfc
Sean Behan <inbox@seanbehan.com>
2014-12-14 15:40:08 -0500

    front end for app

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-from flask import Flask, request
-from library import take_screenshot
+import os
+import json
+from flask import Flask, request, render_template
+from library import take_screenshot, getenv
 
 app = Flask(__name__)
-# app.config['DEBUG'] = True
-
 
 @app.route("/")
 def index():
-    return "Hello World"
-
+    return render_template("index.html")
 
 @app.route("/screenshot")
 def screenshot():
-    if request.args['url']:
-        take_screenshot(request.args['url'])
+    url = str(request.args['url'])
+    if "fullpagescreenshots" in url:
+        return "Sorry, not allowed", 405
 
-    return "Saved"
+    data = {}
+    if request.args['url']:
+        data = take_screenshot(url)
+
+    return json.dumps(data), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+if getenv()=="development":
+    if __name__ == "__main__":
+        app.run(debug=True)
--- library.py
+++ library.py
@@ -1,8 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from hashlib import md5
+import datetime
+import os
+import urlparse
+from collections import OrderedDict
+
+def getenv():
+    if os.environ.get("FLASK_ENV"):
+        return "production"
+    return "development"
+
+def domain():
+    if getenv()=="production":
+        return "http://fullpagescreenshots.com"
+    else:
+        return "http://localhost:5000"
+
+def format_url(url):
+    _url = urlparse.urlparse(url)
+    if '' == _url.scheme:
+        return 'http://%s' % url
+    return url
 
 def take_screenshot(url):
-
+    url = format_url(url)
+    data = OrderedDict()
 
     dcap = dict(DesiredCapabilities.PHANTOMJS)
     dcap["phantomjs.page.settings.userAgent"] = (
@@ -15,9 +38,25 @@ def take_screenshot(url):
 
     driver = webdriver.PhantomJS(desired_capabilities=dcap)
     driver.get(url)
+    # document_title = driver.title
+    data["title"] = driver.title
+
     driver.set_page_load_timeout(10)
     driver.set_window_size(1200, 750)
-    driver.save_screenshot("static/screenshot.png")
+    # document_url = driver.current_url
+    data["url"] = driver.current_url
+    data["screenshot"] = {}
+
+    file_name = "%s.png" % md5(url).hexdigest()
+    file_path = "static/screenshots/%s" % file_name
+    full_url = "%s/%s" % (domain(), file_path)
+
+    driver.save_screenshot(file_path)
     driver.quit()
 
-    return True
+    # data["screenshot"]["file"] = file_name
+    data["screenshot"]["url"] = full_url
+    data["screenshot"]["taken_at"] = str(datetime.datetime.now())
+    data["screenshot"]["size"] = os.path.getsize(str(file_path))
+
+    return data
